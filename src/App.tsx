11ffb1d8ecfe1d7dc16167ea92114acf64a7c379
Way-Maker7 <Denis_Sabou@gmx.de>
2022-07-10 00:02:53 +0200

    input field added

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import './App.css';
 import Gallery from "./components/Gallery";
 import Header from "./components/Header";
 
+
 function App() {
     return (
         <div className="App">
             <Header/>
+            <br/>
+            <br/>
             <Gallery/>
         </div>
     );
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,15 +1,24 @@
-import  characters from '../characters.json'
+import characters from '../characters.json'
 import GalleryItems from "./GalleryItems";
 import "./Gallery.css"
+import InputFeld from "./InputFeld"
 
-export default function Gallery(){
+export default function Gallery() {
 
-    const names = characters.map(c => <GalleryItems character={{name: c.name, imageUrl: c.image, status: c.status,
-    species: c.species, index: c.id}}/>);
+    const names = characters.map(c => <GalleryItems character={{
+        name: c.name, imageUrl: c.image, status: c.status,
+        species: c.species, index: c.id
+    }}/>);
 
-    return(
-        <div className="Gallery">
-            {names}
+    return (
+        <div>
+            <InputFeld />
+            <br/>
+            <br/>
+            <div className="Gallery">
+
+                {names}
+            </div>
         </div>
     )
 
