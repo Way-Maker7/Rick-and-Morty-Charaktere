e797d901538e623afd2b3bfe3fad4b3046646e4f
Way-Maker7 <Denis_Sabou@gmx.de>
2022-07-10 00:55:04 +0200

    logic inputfield

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,23 +1,36 @@
 import characters from '../characters.json'
 import GalleryItems from "./GalleryItems";
 import "./Gallery.css"
-import InputFeld from "./InputFeld"
+import {useState} from "react";
+
 
 export default function Gallery() {
 
-    const names = characters.map(c => <GalleryItems character={{
-        name: c.name, imageUrl: c.image, status: c.status,
-        species: c.species, index: c.id
-    }}/>);
+    const[input, setInput] = useState('');
+
+    const components = characters.filter(c => c.name.toLowerCase().includes(input.toLowerCase()))
+        .map(c => <GalleryItems character={{
+            name: c.name, imageUrl: c.image, status: c.status,
+            species: c.species, index: c.id
+        }}/>);
+
+    {/*
+        const names = characters.map(c => <GalleryItems character={{
+            name: c.name, imageUrl: c.image, status: c.status,
+            species: c.species, index: c.id
+        }}/>);*/
+    }
 
     return (
         <div>
-            <InputFeld />
+            <div>
+            <label htmlFor="name">search by name: </label>
+            <input type="text" id="name" value={input} onChange={event => setInput(event.target.value)}/>
+            </div>
             <br/>
             <br/>
             <div className="Gallery">
-
-                {names}
+                {components}
             </div>
         </div>
     )
